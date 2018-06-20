0dacafbf356ab835887769b713d0210003ff1c00
Francis Nevard <francis.nevard@oxds.co.uk>
2018-06-20 12:08:13 +0100

    wip adding monad

--- src/Acid/Core.hs
+++ src/Acid/Core.hs
@@ -6,41 +6,62 @@ import Generics.SOP
 import GHC.TypeLits
 
 
+{-
+something :: NP I '[Text, String, Int] -> Bool
+something np = runEvent (Event (Proxy :: Proxy "anotherFunc") np)
 
-data Event (n :: Symbol) xs where
-  Event :: (Eventable n, xs ~  FunctionArgs (EventT n)) => (Proxy n) -> (NP I xs) -> Event n xs
-
-
-class (Apply (EventT n) (FunctionArgs (EventT n))) => Eventable  (n :: Symbol) where
-  type EventT n :: k
-  runEvent :: (Proxy n) -> EventT n
-
-
-
-someFunc :: Bool -> Int -> Text
-someFunc = undefined
-
-anotherFunc :: Text -> String -> Int -> Bool
-anotherFunc = undefined
 
 instance Eventable "someFunc" where
   type EventT "someFunc" = Bool -> Int -> Text
-  runEvent _ = someFunc
+  event _ = undefined
 
 instance Eventable "anotherFunc" where
   type EventT "anotherFunc" = Text -> String -> Int -> Bool
-  runEvent _ = anotherFunc
+  event _ = undefined-}
 
-doRunEvent :: Event n xs -> (ApplyResult (EventT n) xs)
-doRunEvent (Event pn np) = apply (runEvent pn) np
 
-something :: NP I '[Text, String, Int] -> Bool
-something np = doRunEvent (Event (Proxy :: Proxy "anotherFunc") np)
 
 
 type family FunctionArgs a = (res :: [*]) where
   FunctionArgs (a -> b) = (a ': FunctionArgs b)
   FunctionArgs (a) = '[]
+
+type family DropResult a where
+  DropResult (a -> b -> c) = a -> DropResult (b -> c)
+  DropResult (a -> b) = a
+
+type family FunctionResult a = (res :: *) where
+  FunctionResult (a -> b) = FunctionResult b
+  FunctionResult a = a
+
+
+class BackendMonad m where
+
+
+data Event (n :: Symbol) xs where
+  Event :: (Eventable n, xs ~  FunctionArgs (EventT n)) => (Proxy n) -> (NP I xs) -> Event n xs
+
+class Eventable  (n :: Symbol) where
+  type EventT n :: *
+  event :: (ApplyM m ) BackendMonad m => (Proxy n) -> DropResult (EventT n) -> m (FunctionResult (EventT n))
+
+
+runEvent :: ( BackendMonad m) => Event n xs -> m (ApplyResult ((DropResult (EventT n) -> (FunctionResult (EventT n)))) xs)
+runEvent (Event pn np) = applyM (event pn) np
+
+class ApplyM m f xs where
+  applyM :: f -> (NP I xs) ->  m (ApplyResult f xs)
+
+
+instance ApplyM m (m a) ('[]) where
+  applyM f _ = f
+
+instance ApplyM m (a -> b) (a ': '[]) where
+  applyM f np = f (unI . hd $ np)
+
+instance (ApplyM m b (x ': xs)) => ApplyM m (a -> b) (a ': x ': xs) where
+  applyM f np = apply (f (unI . hd $ np)) (tl np)
+
 
 
 
