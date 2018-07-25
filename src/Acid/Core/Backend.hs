85c87e8b57a309069d620bf995d6b3621a01d0af
Matchwood <matchwood1@gmail.com>
2018-07-25 20:56:09 +0100

    Start poc for postgres backend

--- src/Acid/Core/Backend.hs
+++ src/Acid/Core/Backend.hs
@@ -2,10 +2,12 @@
 module Acid.Core.Backend (
   module Acid.Core.Backend.Abstract,
   module Acid.Core.Backend.FS,
-  module Acid.Core.Backend.Memory
+  module Acid.Core.Backend.Memory,
+  module Acid.Core.Backend.Groundhog
 
   ) where
 
 import Acid.Core.Backend.Abstract
 import Acid.Core.Backend.FS
 import Acid.Core.Backend.Memory
+import Acid.Core.Backend.Groundhog
