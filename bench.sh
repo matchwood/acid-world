06cbe20ff85c0a34e5eb746c5aab008644670709
Matchwood <matchwood1@gmail.com>
2018-08-04 19:58:52 +0100

    Tidy up a little and add license

--- bench.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-.stack-work/dist/x86_64-linux/Cabal-2.0.1.0/build/acid-world-benchmark/acid-world-benchmark +RTS -xc
-
-# --profile = +RTS -p
-# --trace = +RTS -xc
--- runBench.sh
+++ runBench.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 .stack-work/dist/x86_64-linux/Cabal-2.0.1.0/build/acid-world-benchmark/acid-world-benchmark "$@"
 # pattern eg: -m "glob" "*CBOR/1m*/*"
+# --profile = +RTS -p
+# --trace = +RTS -xc
